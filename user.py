c4c7c52d7934d7c4a38bbf66d90ca4c9b16a6665
Rubyroy12 <ibrakipz7@gmail.com>
2021-05-28 22:00:25 +0300

    create user class

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+    """
+    user class to hold user information
+
+    """
+    def __init__(self,firstname,lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+        
