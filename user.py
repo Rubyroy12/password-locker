03a1c2dde4be82d28d2ddad9a72bb1121d5e0363
Rubyroy12 <ibrakipz7@gmail.com>
2021-05-29 12:10:30 +0300

    add mehod to save username

--- user.py
+++ user.py
@@ -1,11 +1,27 @@
 class User:
     """
-    user class to hold user information
+   class that creaes an instance of a class
 
     """
-    def __init__(self,firstname,lastname):
-        self.firstname = firstname
-        self.lastname = lastname
 
+    user_list=[]
+
+    def __init__(self,username):
+        self.username= username
     
+    def save_user(self):
+        """
+        create method to save usersane
+        """
+
+        User.user_list.append(self)
+
+     
+class Credentials:
+    """
+    create a class that create instance of the class
+
+    """
+    
+
 
